61b5c9f21f92667e0fbe53df687028bd546dc705
Noman Gazi <nomangazix@gmail.com>
2024-11-12 22:18:03 +0600

    update docker file

--- setup-docker-build.sh
+++ setup-docker-build.sh
@@ -4,8 +4,8 @@
 git pull
 
 # Build docker image force with tag "coutnry-json"
-docker build -t country-info-json . # --no-cache
+docker build -t ecoommerce-backend . # --no-cache
 
 # Run the docker container but remove the previous container
 docker rm -f countryjson
-docker run -d --name countryjson -p 5050:5050 country-info-json
+docker run -d --name countryjson -p 5010:5010 ecoommerce-backend
