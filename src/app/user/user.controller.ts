411a9e7d032bf0fb2ef733decfbdd737de3e9a4e
Noman Gazi <nomangazix@gmail.com>
2024-11-12 22:27:28 +0600

    update app/user module update

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { ApiResSuccess } from "../../../utils/ApiResponse";
+import { ApiResSuccess } from "../../utils/ApiResponse";
 
 const getCountryInfo = async (req: Request, res: Response) => {
   ApiResSuccess(res, "Country info fetched successfully", {
